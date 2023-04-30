9a7ebcc90a9969b91023dd20a3249724cde56ac2
rchandra9 <rballa@novaentertainment.com.au>
2023-05-01 08:28:30 +1000

    Working v2 migrations in service4

--- services/service4/stacks/ExampleStack.ts
+++ services/service4/stacks/ExampleStack.ts
@@ -1,29 +1,31 @@
+/** @format */
+
 import { Api, RDS, StackContext } from "sst/constructs";
 
 export function ExampleStack({ stack }: StackContext) {
-  // Create the Aurora DB cluster
-  const cluster = new RDS(stack, "Cluster", {
-    engine: "postgresql11.13",
-    defaultDatabaseName: "CounterDB",
-    migrations: "services/migrations",
-  });
+	// Create the Aurora DB cluster
+	const cluster = new RDS(stack, "Cluster", {
+		engine: "postgresql11.13",
+		defaultDatabaseName: "CounterDB",
+		migrations: "services/migrations",
+	});
 
-  // Create a HTTP API
-  const api = new Api(stack, "Api", {
-    defaults: {
-      function: {
-        bind: [cluster],
-      },
-    },
-    routes: {
-      "POST /": "packages/functions/src/lambda.handler",
-    },
-  });
+	// Create a HTTP API
+	const api = new Api(stack, "Api", {
+		defaults: {
+			function: {
+				bind: [cluster],
+			},
+		},
+		routes: {
+			"POST /": "services/functions/lambda.handler",
+		},
+	});
 
-  // Show the resource info in the output
-  stack.addOutputs({
-    ApiEndpoint: api.url,
-    SecretArn: cluster.secretArn,
-    ClusterIdentifier: cluster.clusterIdentifier,
-  });
+	// Show the resource info in the output
+	stack.addOutputs({
+		ApiEndpoint: api.url,
+		SecretArn: cluster.secretArn,
+		ClusterIdentifier: cluster.clusterIdentifier,
+	});
 }
